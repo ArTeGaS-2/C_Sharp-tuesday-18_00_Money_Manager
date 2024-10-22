2d0cbdb12cf59eb0efdcb7aba3ee74f91c14c202
ArTeGaS-2 <probablytorvik@gmail.com>
2024-10-22 19:54:36 +0300

    Розмітка і клас для транзакцій

--- ВТ-18_00_Money_Manager/MainWindow.xaml.cs
+++ ВТ-18_00_Money_Manager/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Util;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,19 +17,43 @@ using System.Windows.Shapes;
 
 namespace ВТ_18_00_Money_Manager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // ObservableCollection для зберігання транзакцій,
+        // автоматично оновлює UI при додаванні/видаленні елементів
+        private ObservableCollection<Transactions> Transactions_;
+        // Змінна для зберігання поточного балансу
+        private decimal Balance;
         public MainWindow()
         {
             InitializeComponent();
-        }
+            // Ініціалізація колекції транзакцій
+            Transactions_ = new ObservableCollection<Transactions>();
+            // Прив'язка ListView до колекції Transactions
+            TransactionHistoryListView.ItemsSource = Transactions_;
+            // Ініціалізація балансу як нуль
+            Balance = 0;
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        }
+        private void AddTransaction_Click(object sender, RoutedEventArgs e)
         {
 
         }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    // Клас для представлення фінансової транзакції
+    public class Transtaction
+    {
+        // Дата транзакції
+        public string Date { get; set; }
+        // Тип транзакції (Дохід або Витрата)
+        public string Type { get; set; }
+        // Категорія транзакції
+        public string Category {  get; set; }
+        // Сума транзакції
+        public decimal Amount { get; set; }
     }
 }
